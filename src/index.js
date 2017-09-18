a280ae52612a53107ea3089336b2a66afd3116ca
ragexe <ragexe@mail.ru>
2017-09-18 03:09:01 +0300

    additional_4/final: []

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-module.exports = function multiply(first, second) { //TODO
-// function multiply(first, second) {
+module.exports = function multiply(first, second) {
     let result;
 
     let one = getDigitalArrayFromStr(first);
     let two = getDigitalArrayFromStr(second);
 
     result = karaczubaMultiplication(one, two);
-
     result = normalize(result);
 
     return result.reverse().join('');
 };
 
-function karaczubaMultiplication(first, second, level = 3) {
+
+function karaczubaMultiplication(first, second, treshold = 8) {
 
     let n = Math.max(first.length, second.length);
     n += n % 2;
     first = align(first, n);
     second = align(second, n);
 
-    if (n < level){
+    //this code uses simple multiplication with number's complexity below the value
+    //'8' - npm test passes in 18-20ms
+    //'6' - npm test passes in 22-23ms
+    //'3' - npm test passes in 39-40ms
+    if (n < treshold){
         return naiveMultiplication(first, second);
     }
 
@@ -32,10 +35,12 @@ function karaczubaMultiplication(first, second, level = 3) {
     let temp2 = karaczubaMultiplication(first_left, second_left);
     let temp3 = karaczubaMultiplication(sum(first_left, first_right), sum(second_left, second_right));
 
-    return sum(sum(elevate(temp1.slice(0), n),temp2.slice(0)), elevate(diff(temp3.slice(0), sum(temp1.slice(0), temp2)), n/2));
+    //temp1*10^n + temp2 + (temp3 - temp1 - temp2)*10^(n/2)
+    //.slice(0) for clone array;
+    return sum(sum(elevate(temp1.slice(0), n),temp2.slice(0)), elevate(diff(temp3, sum(temp1, temp2)), n/2));
 }
 
-//example '1234' -> [4, 3, 2, 1] //4 *10^0+ 3 *10^1+ 2 *10^2+ 1 *10^3
+//example: '1234' -> [4, 3, 2, 1] //4*10^0 + 3*10^1 + 2*10^2 + 1*10^3
 function getDigitalArrayFromStr(string) {
     let result = string.split('').reverse();
     for (let i = 0; i < result.length; i++) {
@@ -44,6 +49,7 @@ function getDigitalArrayFromStr(string) {
     return result;
 }
 
+//simple multiplication
 function naiveMultiplication(first, second){
     let one = first.slice(0); //clone array
     let two = second.slice(0); //clone array
@@ -57,32 +63,6 @@ function align(arr, n){
         arr = arr.concat([0]);
     }
     return arr;
-}
-
-function normalize(result) {
-    log(result);
-    let base = 10;
-    for (let i = 0; i < result.length; i++) {
-        let temp = Math.floor(result[i]/base);
-        if (temp !== 0){
-            result.push(0);
-            result[i+1] += temp;
-            result[i] -= temp * base;
-        }
-        if (isNaN(result[i])){
-            throw "Invalid data format.";
-        }
-    }
-
-    for (let i = result.length-1; i > 0; i--) {
-        if (result[i] === 0){
-            result.pop();
-        } else {
-            break;
-        }
-    }
-
-    return result;
 }
 
 function elevate(arr, n){
@@ -114,28 +94,28 @@ function diff(arr1, arr2){
     return arr1;
 }
 
-function log(item){
-    console.log(item);
+function normalize(result, base = 10) {
+    for (let i = 0; i < result.length; i++) {
+        let temp = Math.floor(result[i]/base);
+        if (temp !== 0){
+            result.push(0);
+            result[i+1] += temp;
+            result[i] -= temp * base;
+        }
+        if (isNaN(result[i])){
+            throw "Invalid data format.";
+        }
+    }
+
+    //deleting zeros
+    for (let i = result.length-1; i > 0; i--) {
+        if (result[i] === 0){
+            result.pop();
+        } else {
+            break;
+        }
+    }
+
+    return result;
 }
 
-
-
-// log(multiply('5', '10'));
-// //, '50');
-// log(multiply('10000', '10000'));
-// //, '100000000');
-// // log(multiply('100000000', '100000000'));
-// // // '10000000000000000');
-// log(multiply('3286', '5895'));
-
-
-// log(multiply('329568934658432659586', '58379426534596'));
-// // //, '19240045408977038918943543720037256');
-// // log(multiply('439265893465876578346584325', '5837942653434596'));
-// // //, '2564409095663498076999916945221826386307700');
-// // log(multiply('43923423584325', '58379423434596'));
-// // //, '2564224144126429540528307700');
-// // log(multiply('111111111111111111', '22222222222222222222'));
-// // //, '2469135802469135799975308641975308642');
-// // log(multiply('333333333333333333333', '22222222222222222222'));
-// // //, '7407407407407407407325925925925925925926');
